e900a01816edd023e148c14fe7fa8ce42ac1ec2c
mohsabkha <mohsabkha@gmail.com>
2023-08-24 15:26:00 -0500

    DEV_01 initial commit

---
+++ src/main/java/com/craftsentient/craftmind/neuron/Neuron.java
@@ -0,0 +1,7 @@
+package com.craftsentient.craftmind.neuron;
+
+public interface Neuron {
+    Long[] inputs[] = {};
+    Long bias = 0L;
+    Long output = 0L;
+}
---
+++ src/main/java/com/craftsentient/craftmind/neuron/NeuronImpl.java
@@ -0,0 +1,4 @@
+package com.craftsentient.craftmind.neuron;
+
+public class NeuronImpl {
+}
