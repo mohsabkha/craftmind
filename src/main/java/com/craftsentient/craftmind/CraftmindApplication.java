69d09abb83cfa8f18d9af7a26a341060f3d4e47f
mohsabkha <mohsabkha@gmail.com>
2023-08-24 09:29:07 -0500

    initial commit

---
+++ src/main/java/com/craftsentient/craftmind/CraftmindApplication.java
@@ -0,0 +1,13 @@
+package com.craftsentient.craftmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftmindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CraftmindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/craftsentient/craftmind/CraftmindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.craftsentient.craftmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftmindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
