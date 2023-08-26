bb6779dc6d77c30b64e2c8a4c99eb2ccba62f4fe
mohsabkha <mohsabkha@gmail.com>
2023-08-25 19:52:17 -0500

    DEV_01 removed tests for now

--- src/test/java/com/craftsentient/craftmind/neuron/NeuronTest.java
+++ src/test/java/com/craftsentient/craftmind/neuron/NeuronTest.java
@@ -17,30 +17,4 @@ public class NeuronTest {
         inputs.add(1L);
         weights.add(1L);
     }
-
-    @Test
-    public void NeuronConstructorTest(){
-
-    }
-
-    @Test
-    public void createOutputCallTest(){
-        NeuronImpl neuron = new NeuronImpl(inputs,weights,BIAS,0L);
-        neuron.createOutput();
-
-    }
-
-    @Test
-    public void createOutputWithInputsWeightsAndBiasTest(){
-        Long output = NeuronImpl.createOutput(inputs, weights, BIAS);
-
-    }
-
-    @Test
-    public void createOutputWithNeuronTest(){
-        NeuronImpl neuron = new NeuronImpl(inputs,weights,BIAS,0L);
-        neuron = NeuronImpl.createOutput(neuron);
-
-
-    }
 }
