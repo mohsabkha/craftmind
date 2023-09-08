44e811191fb15d2e35f54fb629328bda42d52a0b
Mohammad Sabir Khan <mohsabkha@gmail.com>
2023-09-08 16:59:48 -0500

    DEV_01 setting up the matrix operations

--- src/main/java/com/craftsentient/craftmind/matrix/Matrix.java
+++ src/main/java/com/craftsentient/craftmind/matrix/Matrix.java
@@ -2,6 +2,8 @@ package com.craftsentient.craftmind.matrix;
 
 import com.craftsentient.craftmind.layer.Layer;
 import com.craftsentient.craftmind.neuron.Neuron;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -9,9 +11,24 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Matrix {
+    @Getter
+    @Setter
     private int rows;
+
+    @Getter
+    @Setter
     private int columns;
+
+    @Getter
+    @Setter
     private int[] dimensions = new int[2];
+
+    @Getter
+    @Setter
+    private Double determinant;
+
+    @Getter
+    @Setter
     private ArrayList<Layer> data;
 
     public Matrix(){
@@ -47,79 +64,79 @@ public class Matrix {
         this.dimensions[1] = this.columns;
     }
 
-    public void setNeuronAt(int row, int column, Neuron neuron){
+    public void setNeuronAt(int row, int column, Neuron neuron) {
         data.get(row).getNeuronList().set(column, neuron);
     }
 
-    public void setNeuronAt(int row, int column, int numberOfWeights, Double bias){
+    public void setNeuronAt(int row, int column, int numberOfWeights, Double bias) {
         data.get(row).getNeuronList().set(column, new Neuron(numberOfWeights, bias));
     }
 
-    public void setNeuronAt(int row, int column, int numberOfWeights, Double bias, int max, int min){
+    public void setNeuronAt(int row, int column, int numberOfWeights, Double bias, int max, int min) {
         data.get(row).getNeuronList().set(column, new Neuron(numberOfWeights, bias, max, min));
     }
 
-    public void setNeuronAt(int row, int column, ArrayList<Double> weights, Double bias){
+    public void setNeuronAt(int row, int column, ArrayList<Double> weights, Double bias) {
         data.get(row).getNeuronList().set(column, new Neuron(weights, bias));
     }
 
-    public Neuron getNeuronAt(int row, int column){
+    public Neuron getNeuronAt(int row, int column) {
         return  data.get(row).getNeuronList().get(column);
     }
 
-    public ArrayList<Neuron> getRow(int row){
+    public ArrayList<Neuron> getRow(int row) {
         return data.get(row).getNeuronList();
     }
 
-    public Layer getRowLayer(int row){
+    public Layer getRowLayer(int row) {
         return data.get(row);
     }
 
-    public ArrayList<Neuron> getColumn(int column){
+    public ArrayList<Neuron> getColumn(int column) {
         Layer layer = new Layer();
         data.stream().parallel().forEach(i -> layer.addNeuron(i.getNeuronList().get(column)));
         return layer.getNeuronList();
     }
 
-    public Layer getColumnLayer(int column){
+    public Layer getColumnLayer(int column) {
         Layer layer = new Layer();
         data.stream().parallel().forEach(i -> layer.addNeuron(i.getNeuronList().get(column)));
         return layer;
     }
 
-    public Neuron resetNeuronAt(int row, int column){
+    public Neuron resetNeuronAt(int row, int column) {
         Neuron toRemove = this.getNeuronAt(row, column);
         this.setNeuronAt(row, column, new Neuron());
         return toRemove;
     }
 
-    public void removeRow(int row){
+    public void removeRow(int row) {
         data.remove(row);
         this.rows--;
     }
 
-    public void removeColumn(int column){
+    public void removeColumn(int column) {
         data.stream().parallel().forEach(i -> i.getNeuronList().remove(column));
         this.columns--;
     }
 
-    public void addColumn(){
+    public void addColumn() {
         data.stream().parallel().forEach(i -> i.getNeuronList().add(new Neuron()));
         this.columns++;
     }
 
-    public void addRow(){
+    public void addRow() {
         Layer layer = new Layer();
         layer.generateLayer(this.rows);
         data.add(layer);
         this.rows++;
     }
 
-    public void scale(Double scaler){
+    public void scale(Double scaler) {
         this.data.stream().parallel().forEach(i -> i.getNeuronList().stream().parallel().forEach(x ->  x.setOutput(x.getOutput() * scaler)));
     }
 
-    public void addMatrix(Matrix matrixToAdd){
+    public void addMatrix(Matrix matrixToAdd) {
         if(this.rows != matrixToAdd.rows || this.columns != matrixToAdd.columns){
             throw new ArithmeticException("When adding matrices, rows and columns sizes need to be identical!");
         }
@@ -129,7 +146,7 @@ public class Matrix {
         }).collect(Collectors.toCollection(ArrayList::new));
     }
 
-    public void subtractMatrix(Matrix matrixToAdd){
+    public void subtractMatrix(Matrix matrixToAdd) {
         if(this.rows != matrixToAdd.rows || this.columns != matrixToAdd.columns){
             throw new ArithmeticException("When adding matrices, rows and columns sizes need to be identical!");
         }
@@ -140,29 +157,39 @@ public class Matrix {
         }).collect(Collectors.toCollection(ArrayList::new));;
     }
 
-    public void inverse(){
+
+    public void inverse() {
 
     }
 
-    public void transpose(){
+
+    public void transpose() {
 
     }
 
-    public void exp(){
+
+    public void exp() {
 
     }
 
-    public void multiply(){
+
+    public void multiply() {
 
     }
 
-    public void divide(){
+
+    public void divide() {
+
+    }
+
+
+    public void generateDeterminant() {
 
     }
 
 
     // matrix util function
-    public static boolean isDoubleMatrix(Object matrixData){
+    public static boolean isDoubleMatrix(Object matrixData) {
         return
                 matrixData instanceof ArrayList &&
                 !((ArrayList<?>)matrixData).isEmpty()
