8d9d8271afe569d8ef41703a9dd582b7a538beb6
Mohammad Sabir Khan <mohsabkha@gmail.com>
2023-09-25 00:45:02 -0500

    DEV_01 expanding out loss functions

--- src/main/java/com/craftsentient/craftmind/errorLoss/ErrorLossFunctions.java
+++ src/main/java/com/craftsentient/craftmind/errorLoss/ErrorLossFunctions.java
@@ -1,18 +1,62 @@
 package com.craftsentient.craftmind.errorLoss;
 
 public class ErrorLossFunctions {
-    // squared error or mean squared error
+    public static void lossFunction(DEFAULT_LOSS_FUNCTIONS lossFunction, double[] trueValues, double[] predictedValues){
+        switch (lossFunction){
+            case BINARY_CROSS_ENTROPY_LOSS_FUNCTION -> {}
+            case CATEGORICAL_CROSS_ENTROPY_LOSS_FUNCTION -> {}
+            case CONTRASTIVE_LOSS_FUNCTION -> {}
+            case COSINE_PROXIMITY_LOSS_FUNCTION -> {}
+            case FOCAL_LOSS_FUNCTION -> {}
+            case HINGE_LOSS_FUNCTION -> {}
+            case HUBER_LOSS_FUNCTION -> {}
+            case KL_DIVERGENCE_LOSS_FUNCTION -> {}
+            case LOG_COSH_LOSS_FUNCTION -> {}
+            case L1_LOSS_FUNCTION -> {}
+            case MAPE_LOSS_FUNCTION -> {}
+            case MSE_LOSS_FUNCTION -> {}
+            case MSLE_LOSS_FUNCTION -> {}
+            case NLL_LOSS_FUNCTION -> {}
+            case RANKNET_LOSS_FUNCTION -> {}
+            case SPARSE_CATEGORICAL_CROSS_ENTROPY_LOSS_FUNCTION -> {}
+            case SQUARED_HINGE_LOSS_FUNCTION -> {}
+            case SSIM_LOSS_FUNCTION -> {}
+            case TRIPLET_MARGIN_LOSS_FUNCTION -> {}
+        }
+    }
+    public static void lossFunction(DEFAULT_LOSS_FUNCTIONS lossFunction, double[][] trueValues, double[][] predictedValues){
+        switch (lossFunction){
+            case BINARY_CROSS_ENTROPY_LOSS_FUNCTION -> {}
+            case CATEGORICAL_CROSS_ENTROPY_LOSS_FUNCTION -> {}
+            case CONTRASTIVE_LOSS_FUNCTION -> {}
+            case COSINE_PROXIMITY_LOSS_FUNCTION -> {}
+            case FOCAL_LOSS_FUNCTION -> {}
+            case HINGE_LOSS_FUNCTION -> {}
+            case HUBER_LOSS_FUNCTION -> {}
+            case KL_DIVERGENCE_LOSS_FUNCTION -> {}
+            case LOG_COSH_LOSS_FUNCTION -> {}
+            case L1_LOSS_FUNCTION -> {}
+            case MAPE_LOSS_FUNCTION -> {}
+            case MSE_LOSS_FUNCTION -> {}
+            case MSLE_LOSS_FUNCTION -> {}
+            case NLL_LOSS_FUNCTION -> {}
+            case RANKNET_LOSS_FUNCTION -> {}
+            case SPARSE_CATEGORICAL_CROSS_ENTROPY_LOSS_FUNCTION -> {}
+            case SQUARED_HINGE_LOSS_FUNCTION -> {}
+            case SSIM_LOSS_FUNCTION -> {}
+            case TRIPLET_MARGIN_LOSS_FUNCTION -> {}
+        }
+    }
 
-    // mean absolute error
 
-    // cross entropy or log loss
+    private void binaryCrossEntropy(double[] values){}
+    private void binaryCrossEntropy(double[][] values){}
 
-    // categorical cross entropy loss
 
-    // hinge loss
+    private void categoricalCrossEntropy(double[] values){}
+    private void categoricalCrossEntropy(double[][] values){}
 
-    // huber loss
 
-    // softmax loss
-
+    private void contrastive(double[] values){}
+    private void contrastive(double[][] values){}
 }
