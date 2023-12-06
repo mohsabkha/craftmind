cfa822b434a982b248b93f016bf6d9e047ef6a20
Mohammad Sabir Khan <mohsabkha@gmail.com>
2023-12-06 06:10:57 -0600

    DEV_01 update for bug fix

--- src/main/java/com/craftsentient/craftmind/learningRate/LearningRate.java
+++ src/main/java/com/craftsentient/craftmind/learningRate/LearningRate.java
@@ -6,13 +6,13 @@ import static com.craftsentient.craftmind.learningRate.LearningRateImpl.*;
 public class LearningRate {
     public static double updateLearningRate(DEFAULT_LEARNING_RATE decayFunction, double learningRate, double decayRate, int iteration){
         switch (decayFunction){
-            case EPOCH: {
+            case EPOCH_DECAY: {
                 return epochDecay(learningRate, decayRate, iteration);
             }
-            case STEP: {
+            case STEP_DECAY: {
                 return stepDecay(learningRate, decayRate, iteration);
             }
-            case EXPONENTIAL: {
+            case EXPONENTIAL_DECAY: {
                 // return exponentialDecay();
             }
             case ADAGRAD: {
